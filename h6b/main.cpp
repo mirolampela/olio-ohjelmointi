02694073a72ebb7f236c13779a3a96914726c952
mirolampela <miro.lampela@gmail.com>
2024-12-02 17:26:23 +0200

    Harjoitus 6b tehty

---
+++ h6b/main.cpp
@@ -0,0 +1,13 @@
+#include "exampleclass.h"
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    ExampleClass obj;
+
+    obj.startToWait();
+
+    return a.exec();
+}
